55bdd2cb6b24095255c89454c31bc472fca8894f
Santiago Betancur Duque <sbetancd@proteccion.com>
2025-08-31 17:22:49 -0500

    feat: add RandomImageSSR component and integrate into index page; update Astro config and package dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import node from '@astrojs/node';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+    /* 
+    mode: 'standalone': El servidor se ejecuta de forma independiente, manejando tanto las rutas como los archivos estáticos.
+    mode: 'middleware': El servidor se integra como middleware en un servidor HTTP existente, como Express.
+ */
+  }),
   integrations: [react()]
 });
